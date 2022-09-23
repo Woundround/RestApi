3060a6b32d1a399770322d8d7e8bb21e3f27836c
Alexandr Nomokonov <32986076+4ubi@users.noreply.github.com>
2022-09-23 22:27:07 +0700

    Update QueryHandler.php

--- rest/backend/system/QueryHandler.php
+++ rest/backend/system/QueryHandler.php
@@ -3,61 +3,59 @@
 class QueryHandler{
 
     static public function execute($query = NULL){
-
+            
+        if (!$query) {
+           Logger::status(400);
+          
+           return;
+        }
+        
         $query = explode("/", $query);
         
-        if(count($query) >= 4){
+        if (empty($query) || 4 > count($query)) {
+            Logger::status(400);
+            
+            return;            
+        }
+        
+     
+        $type = $query[0];
+        $class = $query[1];
+        $method = $query[2];
+        $key = $query[3];
 
-            $type = $query[0];
-            $class = $query[1];
-            $method = $query[2];
-            $key = $query[3];
-
-            if($type == "admin"){
-
-                self::log(array(
+        if($type == "admin"){
+             self::log(array(
                     "type" => $type,
                     "class" => $class,
                     "method" => $method,
                     "key" => $key,
-                ));
+            ));
     
-            }
+        }
 
-            if(class_exists($class)){
-
-                $class = new $class;
-                $class->query = $query;
-                if(method_exists($class, $method)){
-
-                    if(API_KEY == $key){
-                            
-                        $class->$method($_POST);
-
-                    }else{
-
-                        Logger::status(403);
-
-                    }
-
-                }else{
-
-                    Logger::status(405);
-
+        if(class_exists($class)){
+            $class = new $class;
+            $class->query = $query;
+                
+            if(method_exists($class, $method)){
+                
+                if(API_KEY !== $key){
+                    Logger::status(403);
+                    
+                    return;
                 }
+                
+                $class->$method($_POST);
 
             }else{
-
                 Logger::status(405);
-
-            }
-
-        }else{
-
-            Logger::status(400);
+             }
             
+            return;
         }
-        
+
+        Logger::status(405);
     }
 
     static public function log($log){}
